285d1462d8bf00c5e2337f8ff4ee84aea1368143
ReedMyers715 <reed.myers@bsu.edu>
2025-09-16 17:09:19 +0000

    Assignment 4 created

---
+++ Assignment4.py
@@ -0,0 +1,7 @@
+import json
+import ssl
+from urllib.request import urlopen
+def main():
+    state = input("Enter two-character state code: ")
+    url =  https://api.weather.gov/points/40.1934,-85.3864
+
