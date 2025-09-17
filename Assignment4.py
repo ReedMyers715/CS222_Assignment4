a84152c01254feeaf7338e297db11ffeae6e1a95
ReedMyers715 <reed.myers@bsu.edu>
2025-09-17 16:31:41 +0000

    tested, doesn't work

--- Assignment4.py
+++ Assignment4.py
@@ -2,7 +2,6 @@ import json
 import ssl
 from urllib.request import urlopen
 def main():
-    state = input("Enter two-character state code: ")
     url =  "https://api.weather.gov/points/40.1934,-85.3864"
     context = ssl._create_unverified_context
     response = urlopen(url, context=context)
