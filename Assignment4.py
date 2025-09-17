978d593a1b22e1b5e87889ce792c45b71b17d391
ReedMyers715 <reed.myers@bsu.edu>
2025-09-17 16:28:04 +0000

    added untested print method

--- Assignment4.py
+++ Assignment4.py
@@ -3,5 +3,13 @@ import ssl
 from urllib.request import urlopen
 def main():
     state = input("Enter two-character state code: ")
-    url =  https://api.weather.gov/points/40.1934,-85.3864
+    url =  "https://api.weather.gov/points/40.1934,-85.3864"
+    context = ssl._create_unverified_context
+    response = urlopen(url, context=context)
+    properties = json.loads(response.read())
+    print(len(properties["features"]))
+    for forecast in properties["features"]:
+        print(forecast["name"]["temperature"]["detailedForecast"])
+
+main()
 
