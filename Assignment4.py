d95069c75b7b7c3595c7964978cdb6db348a4cc0
ReedMyers715 <reed.myers@bsu.edu>
2025-09-17 17:22:37 +0000

    finished assignmnet 4

--- Assignment4.py
+++ Assignment4.py
@@ -3,12 +3,16 @@ import ssl
 from urllib.request import urlopen
 def main():
     url =  "https://api.weather.gov/points/40.1934,-85.3864"
-    context = ssl._create_unverified_context
-    response = urlopen(url, context=context)
-    properties = json.loads(response.read())
-    print(len(properties["features"]))
-    for forecast in properties["features"]:
-        print(forecast["name"]["temperature"]["detailedForecast"])
+    context = ssl._create_unverified_context()
+    response1 = urlopen(url, context=context)
+    data1 = json.load(response1)
+    forcastURL = data1['properties']['forecast']
+    response2 = urlopen(forcastURL, context=context)
+    data2 = json.load(response2)
+    for period in data2['properties']['periods']:
+        print(f"Period: {period['name']}")
+        print(f"Temperature: {period['temperature']}")
+        print(f"Detailed Forecast {period['detailedForecast']}")
 
 main()
 
